a906c86f42d1e85b6752ca4d784b7aeced7df83b
Preechai Mekbungwan <preechaim@gmail.com>
2017-01-24 10:59:15 +0700

    Separated config file

---
+++ config.h
@@ -0,0 +1,16 @@
+#define WIFI_SSID "WIFISSID"
+#define WIFI_PASS "WIFIPASS"
+#define WIFI_PRINT_INTERVAL 500
+
+#define TS_HOST "api.thingspeak.com"
+#define TS_PORT 80
+#define TS_KEY "THINGSPEAKKEY"
+#define TS_INTERVAL 300000
+
+#define AWAKE_TIMEOUT 20000
+
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+#define DHT_PWR_PIN 3 // Override PIN3 Serial RX
+#define DHT_DISABLE 1000
+#define DHT_INTERVAL 2000
--- esp8266-dht22-ts.ino
+++ esp8266-dht22-ts.ino
@@ -1,37 +1,21 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
+#include "config.h"
+
 ADC_MODE(ADC_VCC);
 
 //// DHT
-#define DHT_PIN 2
-#define DHT_TYPE DHT22
-#define DHT_PWR_PIN 3 // Override PIN3 Serial RX
-#define DHT_DISABLE 1000
-#define DHT_INTERVAL 2000
-
 DHT dht(DHT_PIN, DHT_TYPE);
 unsigned long nextRead = DHT_DISABLE + 2000; // wait for sensor to be stabilized
 bool isRead = false;
 
 //// Wifi connection
-#define WIFI_SSID "WIFISSID"
-#define WIFI_PASS "WIFIPASS"
-#define WIFI_PRINT_INTERVAL 500
-
 unsigned long nextWifiReport = 0;
 
 //// Wifi client
 WiFiClient client;
-#define TS_HOST "api.thingspeak.com"
-#define TS_PORT 80
-#define TS_KEY "THINGSPEAKKEY"
-#define TS_INTERVAL 300000
-
 String postStr = "";
 bool isConnected = false;
-
-//// Max awake time
-#define AWAKE_TIMEOUT 20000
 
 //// RTC User Data
 struct {
